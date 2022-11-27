fd92955ad5ab870a2167c91f9f1036742d98c366
Deepak Gowtham <47908891+deepakgowtham@users.noreply.github.com>
2022-11-27 12:59:03 +0530

    Update Join.sql

--- SQL/Join.sql
+++ SQL/Join.sql
@@ -1,4 +1,5 @@
--- Inner join
+--JOIN
+--Inner join
 SELECT cities.name as city, countries.name as country, countries.region
 FROM cities
 INNER JOIN countries
@@ -37,7 +38,7 @@ inner join economies as e
 using(code);
 
 
---- Care should be taken to avoid duplicate entries.
+--- Care should be taken to avoid cross entries
 SELECT name, e.year, fertility_rate, unemployment_rate
 FROM countries AS c
 INNER JOIN populations AS p
@@ -46,3 +47,76 @@ INNER JOIN economies AS e
 ON c.code = e.code
 -- Add an additional joining condition such that you are also joining on year
 	and  p.year= e.year;
+	
+-- Left Join
+-- all the rows from left table and matching rows from right table
+-- left join is also called **left outer join**
+SELECT 
+	c1.name AS city, 
+    code, 
+    c2.name AS country,
+    region, 
+    city_proper_pop
+FROM cities AS c1
+-- Join right table (with alias)
+left join countries as c2 
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+
+-- Right join
+-- all the rows from right table and matching row from left
+-- null values for empty record from right table
+-- left join is more commonly used than right join as both can do same work
+SELECT countries.name AS country, languages.name AS language, percent
+FROM languages
+right JOIN countries
+USING(code)
+ORDER BY language;
+
+-- Join and group by
+select region, avg(gdp_percapita) as avg_gdp
+FROM countries AS c
+LEFT JOIN economies AS e
+USING(code)
+WHERE year = 2010
+-- Group by region
+GROUP BY region;
+
+
+-- Full Join
+-- All the data from both the table
+SELECT name AS country, code, region, basic_unit
+FROM countries
+-- Join to currencies
+inner join currencies 
+USING (code)
+WHERE region = 'North America' 
+	OR name IS NULL
+ORDER BY region;
+
+-- Cross join
+-- Create all possible combination of records
+-- no need to use on or using 
+select name, language
+from countries 
+cross join languages
+
+
+-- self join
+-- join a table with itself
+-- there is no "self join" keyword in sql we need use alias 
+-- you are interested in finding out how much the populations for each country changed from 2010 to 2015.
+SELECT 
+	p1.country_code, 
+    p1.size AS size2010, 
+    p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
+WHERE p1.year 
+-- Filter such that p1.year is always five years before p2.year
+    =(p2.year-5)
+    
+-- here we first joined the table on itself and then eliminated the rows that are not required depending on our condition using where clause
+
+
